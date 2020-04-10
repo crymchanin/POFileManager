b700f13f5bc4c2cfc829259ce65e34d610560df8
Anatoliy <Anatoliy@ANATOLIY-PC>
2020-04-10 15:46:40 +0300

    1) Добавлен проект с реализацией ExternalLib

---
+++ POFileManagerTask/AppHelper.cs
@@ -0,0 +1,10 @@
+﻿using FirebirdSql.Data.FirebirdClient;
+
+
+namespace POFileManagerTask {
+    public static class AppHelper {
+        public static T GetSafeValue<T>(this FbDataReader reader, string fieldName) {
+            return (reader.IsDBNull(reader.GetOrdinal(fieldName))) ? default(T) : (T)reader[fieldName];
+        }
+    }
+}
